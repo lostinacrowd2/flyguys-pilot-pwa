bacad6255733a34f0572f92797b696e8063c0ac6
lostinacrowd2 <114860527+lostinacrowd2@users.noreply.github.com>
2026-04-07 13:20:04 -0400

    Refactor service worker for new cache strategy

--- sw.js
+++ sw.js
@@ -1,38 +1,15 @@
-const CACHE_NAME = 'flyguys-pilot-v1';
-const SHELL_FILES = [
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-// Install: cache the app shell
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES))
-  );
+const CACHE = 'fg-v2';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c =>
+    c.addAll(['/Drone-Job-checklist/', '/Drone-Job-checklist/index.html'])
+  ));
   self.skipWaiting();
 });
-
-// Activate: clean old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )));
 });
-
-// Fetch: serve shell from cache, everything else goes to network
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-  
-  // Only cache our own shell files; let all pilots.flyguys.com 
-  // requests go straight to network (we don't want to interfere)
-  if (url.origin === self.location.origin) {
-    event.respondWith(
-      caches.match(event.request).then((cached) => cached || fetch(event.request))
-    );
-  }
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
